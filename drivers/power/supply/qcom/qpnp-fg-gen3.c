9747ab532ffe733ca3c4d29cf3df91d3c2de6220
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-25 18:30:24 -0800

    power: qpnp-fg-gen3: Don't bail out when debugfs creation fails
    Debugfs creation failure is a trivial error. Don't kill the driver when
    this happens.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -7024,12 +7024,7 @@ static int fg_gen3_probe(struct platform_device *pdev)
 	/* Keep BATT_MISSING_IRQ disabled until we require it */
 	vote(chip->batt_miss_irq_en_votable, BATT_MISS_IRQ_VOTER, false, 0);
 
-	rc = fg_debugfs_create(chip);
-	if (rc < 0) {
-		dev_err(chip->dev, "Error in creating debugfs entries, rc:%d\n",
-			rc);
-		goto exit;
-	}
+	fg_debugfs_create(chip);
 
 #if defined(CONFIG_SOMC_CHARGER_EXTENSION)
 	rc = fg_somc_create_sysfs_entries(chip->dev);
