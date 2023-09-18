CloudedQuartz/kernel_xiaomi_sdm845_wip

Fixup "PM / core: Add error argument to dpm_show_time()"
This fixes commit a00f14b.

Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

drivers/base/power/main.c
@@ -428,7 +428,7 @@ static void dpm_show_time(ktime_t starttime, pm_message_t state, int error, char
 	usecs = usecs64;
 	if (usecs == 0)
 		usecs = 1;
-	pr_info("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
+	pr_info("PM: %s%s%s of devices %s after %ld.%03ld msecs\n",
 		  info ?: "", info ? " " : "", pm_verb(state.event),
 		  error ? "aborted" : "complete",
 		  usecs / USEC_PER_MSEC, usecs % USEC_PER_MSEC);

Miiyo/android_kernel_sony_sdm845

Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

drivers/base/power/main.c
@@ -445,7 +445,7 @@ static void dpm_show_time(ktime_t starttime, pm_message_t state, int error, char
 	usecs = usecs64;
 	if (usecs == 0)
 		usecs = 1;
-	pr_debug("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
+	pr_debug("PM: %s%s%s of devices %s after %ld.%03ld msecs\n",
 		  info ?: "", info ? " " : "", pm_verb(state.event),
 		  error ? "aborted" : "complete",
 		  usecs / USEC_PER_MSEC, usecs % USEC_PER_MSEC);